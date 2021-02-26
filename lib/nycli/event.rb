c17e50a9dcecb064aeafd7fb4d3ec3a25f01d81f
R B <rons9n@gmail.com>
2021-02-25 22:32:55 -0500

    displaying more events

--- lib/nycli/event.rb
+++ lib/nycli/event.rb
@@ -1,5 +1,10 @@
 class NYCLI::Event 
-    attr_accessor :name, :date, :day, :time, :venue, :description
+
+    @@white = "\e[0m"
+    @@green = "\e[1;32m"
+    @@blue = "\e[1;34m"
+
+    attr_accessor :name, :date, :day, :time, :venue, :description, :link 
     @@all = []
 
     def initialize
@@ -17,4 +22,20 @@ class NYCLI::Event
             counter += 1
         end
     end
+
+    # More events displaying:
+
+    def self.more_names
+        counter = 1
+        starter = NYCLI::Scraper.page * 20 - 19
+        more_events = starter
+
+        puts "\n"
+        self.all.each do |event|
+            event = "#{counter}- #{event.name}"
+            puts event if counter == more_events
+            counter += 1
+            more_events += if counter > starter
+            end
+        end
 end
