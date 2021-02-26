6c5f9b765bc6af774604a10fa974b80fc2edd438
R B <rons9n@gmail.com>
2021-02-25 22:09:10 -0500

    set up scraper

--- lib/nycli/scraper.rb
+++ lib/nycli/scraper.rb
@@ -1,3 +1,17 @@
 class NYCLI::Scraper 
+    attr_accessor :url
+    @@page = 1
+
+    def initialize(url = "https://https://www.nyc.com/events/")
+        @url = url 
+    end
+
+    def get_page
+        Nokogiri::HTML(URI.open(self.url))
+    end
+
+    def get_events
+        self.get_page.css(".eventrecords li[itemtype='http://schema.org/Event']")
+    end
 end
 
